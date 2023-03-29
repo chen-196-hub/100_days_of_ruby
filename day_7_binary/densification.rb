ac9dec58b561f31c06d15b63fd950c8e0e3549f0
196 <kankou.chin@gmail.com>
2023-03-30 00:24:02 +0900

    day7-2: binary densification

---
+++ day_7_binary/densification.rb
@@ -0,0 +1,13 @@
+str = "LGTM"
+
+def densification str
+ array = []
+ ary = str.split('')
+ ary.each do |i|
+   binary = i.ord.to_s(2)
+   array << binary
+ end
+ puts array.join(' ')
+end
+
+densification str
