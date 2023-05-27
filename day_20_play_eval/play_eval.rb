610e734eb786a79a61b01aad59ffc32eacb07f41
196 <kankou.chin@gmail.com>
2023-05-27 20:52:46 +0900

    day_20: play eval

--- day_20_play_eval/play_eval.rb
+++ day_20_play_eval/play_eval.rb
@@ -1,23 +1,23 @@
 
 
 
-a = <<-EOF
-def hello 
- puts 'hello ruby'
-end
-EOF
+# a = <<-EOF
+# def hello 
+#  puts 'hello ruby'
+# end
+# EOF
 
-def densification str
-  array = []
-  ary = str.split('')
-  ary.each do |i|
-    binary = i.ord.to_s(2)
-    array << binary
-  end
-  array.join(' ')
- end
+# def densification str
+#   array = []
+#   ary = str.split('')
+#   ary.each do |i|
+#     binary = i.ord.to_s(2)
+#     array << binary
+#   end
+#   array.join(' ')
+#  end
 
-ccc  =  densification a
+# ccc  =  densification a
 
 def analysis arg
   array = []
@@ -28,8 +28,7 @@ def analysis arg
   array.join
  end
 
-# p ccc.class
-p analysis ccc.split(' ')
+test  = "1100100 1100101 1100110 100000 1101000 1100101 1101100 1101100 1101111 100000 1010 100000 1110000 1110101 1110100 1110011 100000 100111 1101000 1100101 1101100 1101100 1101111 100000 1110010 1110101 1100010 1111001 100111 1010 1100101 1101110 1100100 1010"
 
-
-# TODO
+p eval analysis test.split(' ')
+hello
