606e2b6ece3bbd7dc49fa2df71644ba66d8132eb
chen-196-hub <kankou.chin@gmail.com>
2023-06-22 13:06:18 +0900

    day_22: pascal triangle fin

--- day_22_Pascal_s_triangle/main.rb
+++ day_22_Pascal_s_triangle/main.rb
@@ -1,13 +1,18 @@
-
-
 def pascal level
   ary = [[1], [1,1]]
   if level > 2
     (level - 2).times do |l|
-    
+      ary << [1]
+      ary[l + 1].each_cons(2) do |a, b|
+        ary[l + 2] << a + b
+      end
+      ary[l + 2] << 1
     end    
   end
   ary[0..level - 1]
+  
 end
 
-pp pascal 1 
+
+
+pp pascal 5 
